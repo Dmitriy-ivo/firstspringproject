a479d71cc0093168abf2ad21a5e6a982a0f95fe4
Dmitriy-ivo <13diman13@mail.ru>
2021-10-17 22:45:07 +0300

    add 3

--- src/main/java/com/example/firstspringproject/Repository/UserRepository.java
+++ src/main/java/com/example/firstspringproject/Repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.firstspringproject.Repository;
 import com.example.firstspringproject.Entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
 }
---
+++ src/main/java/com/example/firstspringproject/Service/UserService.java
@@ -0,0 +1,76 @@
+package com.example.firstspringproject.Service;
+
+import com.example.firstspringproject.Entity.Role;
+import com.example.firstspringproject.Entity.UserEntity;
+import com.example.firstspringproject.Repository.RoleRepository;
+import com.example.firstspringproject.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+    @PersistenceContext
+    private EntityManager em;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByUsername(username);
+
+        if (userEntity == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return userEntity;
+    }
+
+    public UserEntity findUserById(Long userId) {
+        Optional<UserEntity> userFromDb = userRepository.findById(userId);
+        return userFromDb.orElse(new UserEntity());
+    }
+
+    public List<UserEntity> allUsers() {
+        return (List<UserEntity>) userRepository.findAll();
+    }
+
+    public boolean saveUser(UserEntity user) {
+        UserEntity userFromDB = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDB != null) {
+            return false;
+        }
+
+        user.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return true;
+    }
+
+    public boolean deleteUser(Long userId) {
+        if (userRepository.findById(userId).isPresent()) {
+            userRepository.deleteById(userId);
+            return true;
+        }
+        return false;
+    }
+
+    public List<UserEntity> usergtList(Long idMin) {
+        return em.createQuery("SELECT u FROM User u WHERE u.id > :paramId", UserEntity.class)
+                .setParameter("paramId", idMin).getResultList();
+    }
+}
