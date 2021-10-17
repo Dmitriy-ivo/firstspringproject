a3a931159c8c5c6460e3cc56556e3ef6467d5f20
Dmitriy-ivo <13diman13@mail.ru>
2021-10-17 18:13:39 +0300

    add 3

--- src/main/java/com/example/firstspringproject/Model/User.java
+++ src/main/java/com/example/firstspringproject/Model/User.java
@@ -1,5 +1,14 @@
 package com.example.firstspringproject.Model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@RequiredArgsConstructor
 public class User {
     private long id;
     private String name;
