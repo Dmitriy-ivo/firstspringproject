645fcdb9a96d2a02a2a9cef79fe9b0f3aea18f33
Dmitriy-ivo <13diman13@mail.ru>
2021-10-19 21:06:11 +0300

    add 3

--- src/main/java/com/example/SpringSecurity/FirstspringprojectApplication.java
+++ src/main/java/com/example/SpringSecurity/FirstspringprojectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurtyProjectApplication {
+class SpringSecurityProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurtyProjectApplication.class, args);
+		SpringApplication.run(SpringSecurityProjectApplication.class, args);
 	}
 
 }
